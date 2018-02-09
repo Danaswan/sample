1a532f5c3c14fd87c51e88b44c755ca93f170cf6
dana <dana@qq.com>
2018-02-09 14:45:52 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,17 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Sample App') - Laravel test 001</title>
-    {{--
-    @yield 传了两个参数，第一个参数是该区块的变量名称，第二个参数是默认值，表示当指定变量的值为空值时，使用 Sample 来作为默认值。
-    --}}
+    <title>@yield('title', 'Sample App') - Laravel Test 001</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10"></div>
+    @yield('content')
+    @include('layouts._footer')
+</div>
+
 </body>
 </html>
