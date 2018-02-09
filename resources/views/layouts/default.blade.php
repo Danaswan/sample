740d5896facb6fdb91d2e7de9387a3b4adc8d3d2
dana <dana@qq.com>
2018-02-09 17:34:43 +0800

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,8 @@
 @include('layouts._header')
 
 <div class="container">
-    <div class="col-md-offset-1 col-md-10"></div>
+    <div class="col-md-offset-2 col-md-10"></div>
+    @include('shared._messages')
     @yield('content')
     @include('layouts._footer')
 </div>
